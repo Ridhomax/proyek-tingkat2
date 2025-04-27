739069ba5c3777cd261eb1f6d6c76bc7453dc97b
M. Ridho Masriyan <abuhurairoh172@gmail.com>
2025-04-27 22:25:50 +0700

    first

---
+++ app/buku/page.tsx
@@ -0,0 +1,5 @@
+export default function buku(){
+    return(
+        <h1>Penjual 2</h1>
+    )
+}
---
+++ app/database/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { TextHoverEffect } from "@/components/ui/text-hover-effect";
+
+export default function database(){
+    return (
+    <div className="h-[40rem] flex items-center justify-center">
+      <TextHoverEffect text="DATABASE" />
+    </div>
+    )
+}
