1e077a8ce06ecf4be44f087d210442a30c0a3206
M. Ridho Masriyan <abuhurairoh172@gmail.com>
2025-05-05 07:34:37 +0700

    properties 2

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,9 +1,12 @@
 import { FileUploadDemo } from "@/components/FileUploadDemo";
+import { ThreeDCardDemo } from "@/components2/ThreeDCardDemo";
+
 
 
 export default function properties() {
   return (
     <main>
+      <ThreeDCardDemo/>
       <FileUploadDemo/>
     </main>
   );
